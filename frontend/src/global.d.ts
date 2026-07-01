7ad6e41e8cc647115713c8f79e5e96d2b097b2a5
tobiasgjerstrup <tobiasgjerstrup@gmail.com>
2026-07-01 16:48:03 +0200

    part of the prev. commit :)

--- frontend/src/global.d.ts
+++ frontend/src/global.d.ts
@@ -10,7 +10,9 @@ declare global {
 		pullGitChanges: () => Promise<void>;
 		commitGitChanges: (message: string) => Promise<void>;
 		switchGitBranch: (branchName: string) => Promise<void>;
-		discardGitFile: (filePath: string) => Promise<void>;
+		discardGitFile: (filePath: string, description?: string) => Promise<void>;
+		confirmDiscardGitFile: () => Promise<void>;
+		cancelDiscardGitFile: () => void;
 		refresh(): Promise<void>;
 		toggleDiff: (el: HTMLElement) => void;
 	}
